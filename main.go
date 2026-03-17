879d72693d1e35c8e9cdf71771006c8af93afbb9
Francisco Santos <xikaovs@gmail.com>
2026-03-16 23:48:24 -0300

    feat: Implement a basic REPL loop, add VS Code launch configuration, and ignore `repl.log`.

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		words := cleanInput(text)
+		if len(words) == 0 {
+			continue
+		}
+		fmt.Println("Your command was:", words[1])
+	}
 }
